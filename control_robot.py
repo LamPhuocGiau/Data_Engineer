7ec71024cc59d4a7b0c187b31ceb35a7b1e08bd7
Giau_Lam <lamphuocgiau86@gmail.com>
2024-04-02 14:58:06 +0300

    control robot

--- control_robot.py
+++ control_robot.py
@@ -1,10 +1,16 @@
 class DriveBot:
-    # Update this constructor!
+  # Create a counter to keep track of how many robots were created
+    all_disabled = False
+    latitude = -999999
+    longitude = -999999
+    robot_count = 0
     def __init__(self, motor_speed = 0, direction = 180, sensor_range = 10):
         self.motor_speed = motor_speed
         self.direction = direction
         self.sensor_range = sensor_range
-    
+        # Assign an `id` to the robot when it is constructed by incrementing the counter and assigning the value to `id`
+        DriveBot.robot_count += 1
+        self.id = DriveBot.robot_count
     def control_bot(self, new_speed, new_direction):
         self.motor_speed = new_speed
         self.direction = new_direction
@@ -17,8 +23,10 @@ robot_1.motor_speed = 5
 robot_1.direction = 90
 robot_1.sensor_range = 10
 
-# Create robot_2 here!
 robot_2 = DriveBot(35, 75, 25)
-print(robot_2.motor_speed)
-print(robot_2.direction)
-print(robot_2.sensor_range)
+robot_3 = DriveBot(20, 60, 10)
+
+print(robot_1.id)
+print(robot_2.id)
+print(robot_3.id)
+
